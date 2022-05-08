4a06e804a3fd1fab780732b1f15ee4c8473035ef
Saurabh Gupta <saurabh.gupta1002@gmail.com>
2022-05-08 16:49:01 +0200

    Patch Sampler done

--- Sheet02/Sampler.py
+++ Sheet02/Sampler.py
@@ -1,5 +1,4 @@
--import numpy as np
-
+import numpy as np
 
 class PatchSampler():
     def __init__(self, train_images_list, gt_segmentation_maps_list, classes_colors, patch_size):
@@ -13,8 +12,16 @@ class PatchSampler():
     # provide your implementation
     # should return extracted patches with labels
     def extractpatches(self):
-        pass
-
-    # feel free to add any helper functions
-
-
+        p1 = self.patch_size // 2
+        p2 = (self.patch_size + 1) // 2
+        self.training_patches = []
+        self.training_patches_labels = []
+        for image, segmap in zip(self.train_images_list, self.gt_segmentation_maps_list):
+            h, w, _ = image.shape
+            num_patches = int((w * h) / 4)
+            rows = np.random.randint(p1, h - p2 + 1, num_patches)
+            cols = np.random.randint(p1, w - p2 + 1, num_patches)
+            for r, c in zip(rows, cols):
+                patch = image[r - p1:r + p2, c - p1: c + p2]
+                self.training_patches.append(patch)
+                self.training_patches_labels.append(segmap[r, c])
--- Sheet02/sheet2.py
+++ Sheet02/sheet2.py
@@ -4,10 +4,29 @@ import numpy as np
 import cv2
 import json
 
-
+def read_data(root_folder: str, data_ascii_file: str):
+    data = np.loadtxt(root_folder + data_ascii_file, str)
+    num_images, num_classes = data[0].astype(np.int16)
+    images = []
+    segmaps = []
+    for i in range(num_images):
+        image = cv2.imread(root_folder + data[i + 1, 0])
+        images.append(image)
+        segmap = cv2.imread(root_folder + data[i + 1, 1], 0)
+        segmaps.append(segmap)
+    
+    return num_images, num_classes, images, segmaps
 
 def main():
+    # Read data
+    train_n, train_k, train_imgs, train_segmaps = read_data("images/", "train_images.txt")
+    test_n, test_k, test_imgs, test_segmap = read_data("images/", "test_images.txt")
+    
+    # Sample patches from training data
+    patch_size = 16
+    train_classes = np.arange(train_k)
+    train_patch_sampler = PatchSampler(train_imgs, train_segmaps, train_classes, patch_size)
+    train_patch_sampler.extractpatches()
 
-# provide your implementation for the sheet 2 here
-
-
+if __name__ == "__main__":
+    main()
